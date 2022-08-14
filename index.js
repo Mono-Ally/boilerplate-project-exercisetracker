f803e411d6900bc0538b130db823735b4f827bbe
Mogamat Nur Ally <92162895+Mono-Ally@users.noreply.github.com>
2022-08-14 12:45:37 +0000

    added save user

--- index.js
+++ index.js
@@ -3,13 +3,33 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+let mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI);
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+//Mongoose schemas
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  username: String
+})
+let userModel = mongooe.model("user",userSchema);
 
+app.post("/api/users", async function(req,res,next){
+  let username = req.body.username;
+  console.log(username);
+  let toSave = {
+    username: username
+  };
+  let newUser = new userModel(toSave);
+  await newUser.save();
+  console.log(newUser);
+  res.json(newUser);
+});
 
 
 
